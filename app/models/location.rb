40a67efe660d1ca4233232f8e30f8317b6c930cd
Rodney Ditchfield <rodney.ditch@hotmail.com>
2021-01-27 22:32:23 +1100

    rails side api  working for now will begin working on react CRUD

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,12 +1,15 @@
 class Location < ApplicationRecord
+  # Geocoding goodness
+  geocoded_by :address
+  after_validation :geocode
   # Validations
   validates :name, :location_type, :address, presence: true
 
-  validates :description, length: { maximum: 5000 },presence: :true
+  validates :description, length: { maximum: 5000 }, presence: :true
 
   # Relationships
   belongs_to :location_type
-  
+  belongs_to :user
   has_many :reviews, dependent: :destroy
   # Allows many facilities to be listed for each location
   has_many :location_facilities, dependent: :destroy
@@ -16,7 +19,17 @@ class Location < ApplicationRecord
   has_many :favourites, dependent: :destroy
   has_many :users, through: :favourites
 
-  # Geocoding goodness
-  geocoded_by :address
-  after_validation :geocode
+  def transform_json
+    return{
+      user: user.username,
+      location_type: location_type.name,
+      name: name,
+      address: address,
+      description: description,
+      longitude: longitude,
+      latitude: latitude,
+      posted: created_at,
+      edited: updated_at
+    }
+  end
 end
