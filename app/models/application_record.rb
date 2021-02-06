4225c2efd21adc37f4df6b927c34914dc70aa599
Rodney Ditchfield <rodney.ditch@hotmail.com>
2021-02-06 16:11:08 +1100

    image url playing nice now, had to do things i shouldnt have so will need to ask for advice on monday

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationRecord < ActiveRecord::Base
+  include Rails.application.routes.url_helpers
   self.abstract_class = true
   # the cleanest way i could think of to get the names for facilities from the join table
   def get_facilities(location)
@@ -11,10 +12,20 @@ class ApplicationRecord < ActiveRecord::Base
 
     facilities
   end
- def image_check (loc)
-  byebug
-  loc.image.attached ? image : nil
- end
+
+  def get_image_url
+    url_for(image)
+  end
+
+  def image_check(location)
+    if location.reviews.any?
+      location.reviews.map do |review|
+        review.image.attached? ? review.get_image_url : nil
+      end
+
+    end
+  end
+
   # Makes the JSON request easier to work with on the React side this is called on the Location object before transmitting it to extract details relating to the location so instead of say having user come through as an integer it comes through with the string
   def transform_json
     facility_array = get_facilities(self)
@@ -32,7 +43,7 @@ class ApplicationRecord < ActiveRecord::Base
       faved: false,
       location_facilities_attributes: facility_array,
       google: Rails.application.credentials.dig(:google_maps, :api_key),
-       #image_url: url_for(image_check(reviews))
+      image_url: image_check(self)
     }
   end
 end
