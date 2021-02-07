fd99ab6073db0ecf764ab1222da1ebd3040d11b0
Rodney Ditchfield <rodney.ditch@hotmail.com>
2021-02-07 17:55:38 +1100

    moving transform json method back to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,23 +3,5 @@
 class ApplicationController < ActionController::API
   # good 'ol knock for auth
   include Knock::Authenticable
-  def transform_json
-    facility_array = get_facilities(self)
-    {
-      id: id,
-      location_type_name: location_type.name,
-      name: name,
-      address: address,
-      description: description,
-      longitude: longitude,
-      latitude: latitude,
-      posted: created_at,
-      edited: updated_at,
-      reviews: reviews.map(&:get_reviews),
-      faved: false,
-      location_facilities_attributes: facility_array,
-      google: Rails.application.credentials.dig(:google_maps, :api_key)
-      
-    }
-  end
+ 
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -28,5 +28,23 @@ class ApplicationRecord < ActiveRecord::Base
 
   # Makes the JSON request easier to work with on the React side this is called on the Location object before transmitting it to extract details relating to the location so instead of say having user come through as an integer it comes through with the string
   
-
+  def transform_json
+    facility_array = get_facilities(self)
+    {
+      id: id,
+      location_type_name: location_type.name,
+      name: name,
+      address: address,
+      description: description,
+      longitude: longitude,
+      latitude: latitude,
+      posted: created_at,
+      edited: updated_at,
+      reviews: reviews.map(&:get_reviews),
+      faved: false,
+      location_facilities_attributes: facility_array,
+      google: Rails.application.credentials.dig(:google_maps, :api_key)
+      
+    }
+  end
 end
